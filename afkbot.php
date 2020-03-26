d781eda41592755895212f1727bb152218c2423c
foerkede <foerkede@aol.com>
2020-03-26 18:58:01 +0100

    parenthesis error

--- afkbot.php
+++ afkbot.php
@@ -6,12 +6,10 @@ $afk = array();
 $excludeChannels = explode(',', getenv('EXCLUDE_CHANNELS'));
 $afkChannel = getenv('AFK_CHANNEL');
 $idleTime = 1000*60*getenv('IDLE_MINUTES');
-//var_dump($ts->clientList("-times -away")["data"][3]["client_idle_time"]);
-//exit();
 
 while(1){
 	foreach($ts->clientList("-times -away")["data"] as $c){
-		if ($c["client_away"] == 1 || !in_array($c["cid"], $excludeChannels) && ($c["client_idle_time"] > $idleTime)) {
+		if (!in_array($c["cid"], $excludeChannels) && ($c["client_away"] == 1 || ($c["client_idle_time"] > $idleTime))) {
 			$afk[$c["clid"]] = $c["cid"];
 			$ts->clientMove($c["clid"], $afkChannel);
 		}
