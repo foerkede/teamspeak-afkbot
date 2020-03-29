a45c23eba5a70ad78482f4d1af125fe98cf1eaff
foerkede <foerkede@aol.com>
2020-03-29 17:10:06 +0200

    exit if server not reachable

--- connection.php
+++ connection.php
@@ -3,6 +3,12 @@ require __DIR__ . '/vendor/autoload.php';
 
 class Connection {
 	public static function connect() {
+		$connection = @fsockopen('udp://' . getenv('SERVER_ADDRESS'), getenv('QUERY_PORT'));
+    if (! is_resource($connection)) {
+        error_log('Server is not responding\n');
+				exit(1);
+    }
+		fclose($connection);
 		$ts = new ts3admin(getenv('SERVER_ADDRESS'), getenv('QUERY_PORT'));
 		$ts->connect();
 		$ts->login(getenv('QUERY_USERNAME'), getenv('QUERY_PASSWORD'));
